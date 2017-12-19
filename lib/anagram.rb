6ede01587ae1279703f222d7914e1edfa0cb3759
Neil <neilmarkoliverryan@gmail.com>
2017-12-18 19:42:17 -0800

    completed code challenge

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(items_to_array)
+    match = []
+    items_to_array.each do |i|
+      i_to_sort = i.split("").sort
+      if i_to_sort == self.word.split("").sort
+        match << i
+      end
+    end
+    match
+  end
+
+end
